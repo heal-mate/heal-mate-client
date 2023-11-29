71cee4fac902565028af0261e7a31d3ab5f783d9
ggsno <ogs0518@gmail.com>
2023-11-29 11:47:27 +0900

    Feat: #10 보낸 매치 카드 컴포넌트에 성사/거절 상태 분기 추가

--- src/components/CardsSent.tsx
+++ src/components/CardsSent.tsx
@@ -1,3 +1,4 @@
+import { matchStatusDict } from "@/service/apis/match.type";
 import Cards from "./Cards";
 import { useMatchesSent } from "./Cards.hooks";
 
@@ -9,21 +10,43 @@ export default function CardsSent() {
       <Cards
         matchUserInfos={matchesSent?.map((e) => ({
           ...e,
-          buttons: [
-            {
-              text: "취소하기",
-              theme: "outlined",
-              onClickCallback: async () => cancelMatch({ matchId: e.matchId }),
-            },
-            {
-              text: "요청응답대기",
-              theme: "contained",
-              disabled: true,
-              onClickCallback: () => {
-                return null;
-              },
-            },
-          ],
+          buttons:
+            e.status === matchStatusDict.waiting
+              ? [
+                  {
+                    text: "취소하기",
+                    theme: "outlined",
+                    onClickCallback: async () =>
+                      cancelMatch({ matchId: e.matchId }),
+                  },
+                  {
+                    text: "요청응답대기",
+                    theme: "contained",
+                    disabled: true,
+                    onClickCallback: () => {
+                      return null;
+                    },
+                  },
+                ]
+              : e.status === matchStatusDict.accepted
+                ? [
+                    {
+                      text: "성사된 요청",
+                      theme: "contained",
+                      disabled: true,
+                      onClickCallback: () => null,
+                    },
+                  ]
+                : e.status === matchStatusDict.rejected
+                  ? [
+                      {
+                        text: "거절된 요청",
+                        theme: "contained",
+                        disabled: true,
+                        onClickCallback: () => null,
+                      },
+                    ]
+                  : [],
         }))}
         {...rest}
       />
