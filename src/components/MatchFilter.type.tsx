37769cedb6a49027a62030e388a4fcb54039d715
Dahyeon Lee <dada4202@gmail.com>
2023-12-06 21:16:47 +0900

    Fix: location관련 코드 수정

--- src/components/MatchFilter.type.tsx
+++ src/components/MatchFilter.type.tsx
@@ -22,16 +22,3 @@ export type CheckedExercise = {
   squat: boolean;
   deadLift: boolean;
 };
-
-export const locations = [
-  "잠실",
-  "송파",
-  "성수",
-  "삼성",
-  "청담",
-  "역삼",
-  "대치",
-  "개포",
-] as const;
-
-export type Location = (typeof locations)[number];
